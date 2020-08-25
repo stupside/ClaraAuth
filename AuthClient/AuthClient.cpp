2483b675b069d5cfd745783bd8d3a1f8cb7341a0
Kilian Houpeurt <kilian.houpeurt@gmail.com>
2020-08-25 21:41:47 +0200

    Added Colors

--- AuthClient/AuthClient.cpp
+++ AuthClient/AuthClient.cpp
@@ -1,12 +1,31 @@
 // AuthClient.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+//the following are UBUNTU/LINUX, and MacOS ONLY terminal color codes.
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
 #include "AuthLib.h"
 
 #include <Windows.h>
 #include <iostream>
 
-#define PRODUCT_CODE ("7c9272aa041e4f1e87df88587545f495")
+#define PRODUCT_CODE ("c3063d329f63468e95c477c182db1c4a")
 
 int main()
 {
@@ -21,19 +40,23 @@ int main()
 
     // THEN YOU CAN DO WHATEVER YOU WANT
     if (AuthSucceed) {
-        cout << "------------ Auth Succeed ------------" << endl;
+
+        if (Response.Error.m_succeed) { // Key was Expired or Product Paused / Detected / Maintenance.
+            cout << GREEN << "------------ Auth Succeed ------------" << RESET << endl;
+        }
+        else {
+            cout << BOLDRED << "------------ Auth Failed ------------" << RESET << endl;
+        }
+
         cout << "Error: " << Response.Error.m_error << endl;
 
         cout << "Expiry: " << Response.LicenseKey.m_expiry << endl;
         cout << "Product: " << Response.Product.m_name << endl;
         cout << "Package: " << Response.Package.m_name << endl;
-
-        if (!Response.Error.m_succeed)
-            return 0; // Key was Expired or Product Paused / Detected / Maintenance.
     }
     else
     {
-        cout << "------------ Auth Failed ------------" << endl;
+        cout << BOLDRED << "------------ Auth Failed ------------" << RESET << endl;
         cout << "Error: " << Response.Error.m_error << endl;
     }
     Sleep(30000);
